655420c6070895a5bde5e68b60fbb1a4e2d83fe2
channel960608 <channel960608@gmail.com>
2017-04-18 17:23:25 +0800

    第二章，代码完成，需要修改bug

---
+++ src/ex02/pyrmont/Constants.java
@@ -0,0 +1,10 @@
+package ex02.pyrmont;
+
+import java.io.File;
+
+/**
+ * Created by channel on 2017/4/18.
+ */
+public class Constants {
+    public static String WEB_ROOT=System.getProperty("user.dir")+ File.separator+"webroot";
+}
